335d78206bbf9db5829d364f778a9fb744d0ce42
root <root@thomas.cloud-fortress.com>
2014-04-12 14:52:48 -0400

    refactored application load to be cleaner

--- run_server.py
+++ run_server.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 import os
 import sys
+import logging
 
-# do some magic if we are running wsgi
+def setup():
+    # config log format
+    format="%(asctime)-21s %(levelname)s %(name)s (%(funcName)-s) " + \
+           "%(process)d:%(thread)d - %(message)s"
+    logging.basicConfig(level=logging.DEBUG,
+                        format=format,
+                        filename='server.log')
+    
+    # global variables config
+    application.config.load_config('blog.cfg')
+    
+    # setup the blog server
+    from blog.controllers import *
+
+# mod_wsgi setup
 try:
     import mod_wsgi
 
@@ -11,29 +26,15 @@ try:
 
     # Add working directory to paths
     sys.path.append(os.path.dirname(__file__))
+
+    from blog import application
+ 
+    setup()
+
+# bottle setup
 except:
-    pass
-
-from blog import application
-import logging
-
-# config log format
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)-21s %(levelname)s %(name)s (%(funcName)-s) %(process)d:%(thread)d - %(message)s',
-                    filename='server.log')
-
-# global variables config
-application.config.load_config('blog.cfg')
-
-# setup the blog server
-from blog.controllers import *
-
-# start the bottle server if we aren't running as wsgi
-
-try:
-    import mod_wsgi
-    pass
-except:
-    host = application.config.get('server.host', '0.0.0.0')
-    port = application.config.get('server.port', '8080')
+    from blog import application
+    setup()
+    host = application.config.get('bottle.host', '0.0.0.0')
+    port = application.config.get('bottle.port', '8080')
     application.run(host=host, port=port)
